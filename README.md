426e223ec20c4054db84b285ca46e7cb193707e9
Olivia <oliviapenycoblentz@gmail.com>
2017-05-08 16:06:46 +0200

    add readme, index, css

---
+++ README.md
@@ -0,0 +1,4 @@
+PROJET de mise en Page Responsive
+Rendu Visuel du site TWIMH avant insertion de PHP
+Utilisation de Bootstrap en cours
+
---
+++ js/script.js
@@ -0,0 +1,35 @@
+var testApp = angular.module('app',[]);
+
+testApp.controller('testCtrl', function($scope){
+  $scope.updateArticles = false;
+});
+
+
+//   $('.li_index').on('click', function(){
+//     $('.caroussel-display').css('visibility', 'hidden');
+// });
+
+
+
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+for (i = 0; i < slides.length; i++) {
+   slides[i].style.display = "none";
+}
+slideIndex++;
+if (slideIndex> slides.length) {slideIndex = 1}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
